7cb3c6d61c8e2c074fac7d9a37b7a5916622f80e
João de Assis da Silva Carneiro <jonjon667@hotmail.com>
2019-04-05 01:33:34 -0300

    Add files via upload

--- list2.py
+++ list2.py
@@ -2,13 +2,66 @@ from random import randint
 import time
 
 
+
+def bubbleSort(roger):
+    start = time.time()
+    lst = roger
+    nElements = len(lst)-1
+    while not order:
+        for i in range(nElements):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1],lst[i]       
+    end = time.time()   
+    elapsed = end - start
+    return [lst,elapsed]
+
+def selectionSort(roger):
+    start = time.time()
+    alist = roger
+    for i in range(len(alist)):
+        minPosition = i
+        for j in range(i+1, len(alist)):
+            if alist[minPosition] > alist[j]:
+                minPosition = j   
+        temp = alist[i]
+        alist[i] = alist[minPosition]
+        alist[minPosition] = temp
+    end = time.time()   
+    elapsed = end - start               
+    return [alist,elapsed]
+
+def insertionSort(roger):
+    start = time.time()
+    alist = roger
+    for i in range(1,len(alist)):
+        current = alist[i]
+        while i>0 and alist[i-1]>current:
+            alist[i] = alist[i-1]
+            i = i-1
+            alist[i] = current
+    end = time.time()   
+    elapsed = end - start        
+    return [alist,elapsed]
+
+def sortTime(funcao,n):
+    relacao = []
+    for i in range(n):
+        vetor = []
+        for j in range(i):
+            value = randint(0, 10)
+            vetor.append(value)
+        tempo = funcao(vetor)
+        relacao.append([tempo[1],i])
+    return relacao
+
+sortTime(insertionSort,10)
+
+
 def PrintBubbleSort(roger):
     start = time.time()
     lst = roger
     nElements = len(lst)-1
-    order = False
     while not order:
-        order = True
         for i in range(nElements):
             pointer = " "
             if lst[i] > lst[i+1]:
@@ -55,13 +108,11 @@ def PrintInsertionSort(roger):
     y=0
     z=0
     for i in range(1,len(alist)):
-        troca=False
         current = alist[i]
         while i>0 and alist[i-1]>current:
             string = " "
             alist[i] = alist[i-1]
             i = i-1
-            troca = True
             alist[i] = current
             print(alist)
             for j in range(0,i):
